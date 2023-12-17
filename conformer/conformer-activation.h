99542a36a3d0c245474098b899489a4ce1725a70
Wenqing Yang <91286947+Merlin0513@users.noreply.github.com>
2023-12-17 14:28:09 -0500

    Update conformer-activation.h

--- conformer/conformer-activation.h
+++ conformer/conformer-activation.h
@@ -1,7 +1,6 @@
 #ifndef KALDI_CONFORMER_ACTIVATION_H_
 #define KALDI_CONFORMER_ACTIVATION_H_
 
-#include "conformer-utils.h"
 #include "conformer-embedding.h"
 #include "matrix/kaldi-matrix.h"
 #include "cudamatrix/cu-matrix-lib.h"
